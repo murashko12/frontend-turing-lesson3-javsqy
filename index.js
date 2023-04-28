c756e12c700ae39cb5454e3a0f6e2195fb19170c
murashko12 <45760964+murashko12@users.noreply.github.com>
2023-04-29 00:23:30 +0300

    buttons show message

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 import './style.css';
+
+const addButton = document.querySelector('.add__button') //
+const buttons =  document.querySelectorAll('.button')
+const childDiv = document.querySelector('.child')
+
+function showMes() {
+  alert('Ура, нажал!')
+}
+
+
+
+buttons.forEach((button) => {
+  button.addEventListener('click', showMes)
+})
