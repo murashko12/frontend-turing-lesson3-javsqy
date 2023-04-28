5ab586af49c3944bed265f2fbc5d3d53b456ec2f
murashko12 <45760964+murashko12@users.noreply.github.com>
2023-04-29 00:47:04 +0300

    final

--- index.js
+++ index.js
@@ -5,11 +5,20 @@ const buttons =  document.querySelectorAll('.button')
 const childDiv = document.querySelector('.child')
 
 function showMes() {
-  alert('Ура, нажал!')
+  alert('Ура нажал!')
 }
 
+function addNewButton() {
+  const newButton = document.createElement('button')
+  newButton.classList.add('button')
+  childDiv.appendChild(newButton)
+  newButton.innerHTML = 'Новая кнопка'
+  newButton.addEventListener('click', showMes)
+}
 
+addButton.addEventListener('click', addNewButton)
 
 buttons.forEach((button) => {
   button.addEventListener('click', showMes)
 })
+
